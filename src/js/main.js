a91aa52da4f75772a85617a87e4e5b2d48a68e84
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2024-12-03 14:12:13 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+  var siteSticky = function () {
+    $(".js-sticky-header").sticky({ topSpacing: 0 });
+  };
+  siteSticky();
+
+  var siteMenuClone = function () {
+	// too add content to mobile menu
+    $(".js-clone-nav").each(function () {
+      var $this = $(this);
+      $this
+        .clone()
+        .attr("class", "site-nav-wrap")
+        .appendTo(".site-mobile-menu-body");
+    });
+
+	// to toggle mobile and desktop menus
+    $(window).resize(function () {
+      var $this = $(this),
+        w = $this.width();
+
+      if (w > 768) {
+        if ($("body").hasClass("offcanvas-menu")) {
+          $("body").removeClass("offcanvas-menu");
+        }
+      }
+    });
+
+	// open mobile menu
+    $("body").on("click", ".js-menu-toggle", function (e) {
+      var $this = $(this);
+      e.preventDefault();
+
+      if ($("body").hasClass("offcanvas-menu")) {
+        $("body").removeClass("offcanvas-menu");
+        $this.removeClass("active");
+      } else {
+        $("body").addClass("offcanvas-menu");
+        $this.addClass("active");
+      }
+    });
+
+    // click outisde offcanvas (mobile menu)
+    $(document).mouseup(function (e) {
+      var container = $(".site-mobile-menu");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("offcanvas-menu")) {
+          $("body").removeClass("offcanvas-menu");
+        }
+      }
+    });
+  };
+  siteMenuClone();
+});
